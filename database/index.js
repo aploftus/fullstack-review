d5761bd574a7e253b12d57c87c6981df4d7e1d55
aploftus <aploftus@users.noreply.github.com>
2017-11-08 15:33:31 -0800

    Save repo data to database, handle duplicates by updating record

--- database/index.js
+++ database/index.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  id: Number,
+  id: {
+    type: Number,
+    unique: true
+  },
   name: String,
   forks: Number,
   owner_id: Number,
@@ -14,10 +17,43 @@ let repoSchema = mongoose.Schema({
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (repos) => {
+  // This function should save a repo or repos to the MongoDB
+  for (var i = 0; i < repos.length; i++) {
+    let repo = repos[i];
+    let repoEntry = new Repo({
+      id: repo.id,
+      name: repo.name,
+      forks: repo.forks,
+      owner_id: repo.owner.id,
+      owner: repo.owner.login
+    });
+
+    // document-save returns a promise
+    repoEntry.save((err, repoEntry) => {
+      // if the record already exists
+      if (err) {
+        // update the record rather than drop it
+        throw repoEntry;
+      }
+      console.log('repoEntry ', repoEntry.name, ' has been saved');
+    })
+    .catch(({id, name, forks, owner_id, owner}) => {
+      let newParams = {
+        name: name,
+        forks: forks,
+        owner_id: owner_id,
+        owner: owner
+      };
+      // when we update, we update the "table", right?
+      Repo.update({id: id}, newParams, (err, raw) => {
+        if (err) {
+          console.error('unable to update repo record');
+        }
+        console.log('raw response from mongo is ', raw);
+      })
+    })
+  }
 }
 
 module.exports.save = save;
