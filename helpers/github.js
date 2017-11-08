d8494050168b9a46d0df41972aeb357a3e0bb135
aploftus <aploftus@users.noreply.github.com>
2017-11-08 14:58:31 -0800

    Fetch repo data from github api

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,25 @@
 const request = require('request');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
-  // TODO - Use the request module to request repos for a specific
-  // user from the github API
+let getReposByUsername = (username) => {
 
-  // The options object has been provided to help you out, 
-  // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: 'http://api.github.com/repos/' + username + '/repos',
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
+      'Authorization': `token ${config.TOKEN}`,
+      'Accept': 'application/vnd.github.v3+json'
     }
   };
 
+  let onResponse = (error, response, body, callback) => {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body);
+      callback(info);
+    }
+  }
+
+  request(options, onResponse);
 }
 
 module.exports.getReposByUsername = getReposByUsername;
