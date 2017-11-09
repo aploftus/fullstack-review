e03499693ebac7254d2e6c3d962992064f0ddd59
aploftus <aploftus@users.noreply.github.com>
2017-11-09 10:13:55 -0800

    Enable repo title link to repo

--- client/src/components/RepoListEntry.jsx
+++ client/src/components/RepoListEntry.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const RepoListEntry = ({repo}) => (
   <tr>
-    <td>{repo.name}</td>
+    <td><a href={"https://github.com/" + repo.owner + "/" + repo.name}>{repo.name}</a></td>
     <td>{repo.owner}</td>
     <td>{repo.forks}</td>
   </tr>
