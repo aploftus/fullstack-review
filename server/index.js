ee49aad0249d5ed25bbeeb761cd81e7283f0b1d7
aploftus <aploftus@users.noreply.github.com>
2017-11-09 14:16:30 -0800

    Fix template literal syntax

--- server/index.js
+++ server/index.js
@@ -31,6 +31,6 @@ app.get('/repos', function (req, res) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log(`listening on port ${app.get('port')`);
+  console.log(`listening on port ${app.get('port')}`);
 });
 
